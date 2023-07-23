179793f0c0ad0dc9961c3b25d2d72c1c7480c813
k.p.vergis <k.p.vergis@gmail.com>
2023-07-23 21:36:23 +0300

    wert

--- sets_dictionaries/fridge/meal_planner.py
+++ sets_dictionaries/fridge/meal_planner.py
@@ -6,4 +6,26 @@ for index,key in enumerate(recipes):
     #print(index, key,recipes[key])
     #print(index, key)
     display_dict[str(index+1)]=key
-print(display_dict)
+
+while True:
+    print(f"please chose a recipe")
+    print("-"*20)
+    
+    for key,value in display_dict.items():
+        print(f"{key} : {value}")
+    choice=input(":> ") 
+    if choice=="0":
+        break
+    elif choice in display_dict:
+        selectedItem=display_dict[choice]
+        print(f"you have selected {selectedItem}")
+        print(f"Checking ingrediants...")
+        ingredients=recipes[selectedItem]
+        print(ingredients)
+        for food_item,requiredQuantity in ingredients.items():
+            quantityInPantry=pantry.get(food_item,0) #to mhden mpainei gia thn periptwsh pou den brethei           
+            if requiredQuantity>quantityInPantry:
+                quantityToBuy=requiredQuantity-quantityInPantry
+                print(f"\tYou need to buy {quantityToBuy} of {food_item} ")    
+            else:
+                print(f"\tYou have the nessesary quanity of {food_item}")
